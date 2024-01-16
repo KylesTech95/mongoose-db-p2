d74c715dfae3ec27a1be8983a8f8e47af329d906
kylestech95 <kylestech95@outlook.com>
2024-01-16 09:53:43 -0500

    feat: create person schema

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 // connecting mongoose
-mongoose.connect(process.env.MONGO_URI,{userNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true})
+// create person schema
 let Person;
-
+const Schema = mongoose.Schema;
+const personSchema = new Schema({
+  name:{type:String, required:true},
+  age:Number,
+  favoriteFoods:[String]
+})
+// store model in variable called Person
+Person = mongoose.model("Person",personSchema)
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
